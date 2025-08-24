d7a631093f7a00c7f34b9a4a5e5c4d5878e8ef61
Vinay Satalkar <68776248+VINAYSATALKAR@users.noreply.github.com>
2025-08-24 22:18:03 +0530

    final trucks done

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import { FaTruck, FaTools, FaDolly, FaTrash } from "react-icons/fa";
-import servicesBg from "/services2.jpg"; // put your background image in /public
+import servicesBg from "/hero.jpg"; // put your background image in /public
 
 const services = [
   {
@@ -9,31 +9,28 @@ const services = [
     description: "Clearing debris and waste after construction projects efficiently.",
     bg: "bg-blue-500",
   },
-  {
-    icon: <FaTools className="text-4xl text-white" />,
-    title: "Driveway Preparation",
-    description: "Preparing driveways with care and precision for any project.",
-    bg: "bg-green-500",
-  },
-  {
-    icon: <FaDolly className="text-4xl text-white" />,
-    title: "Equipment Rental & Transport",
-    description: "Renting and transporting heavy equipment safely.",
-    bg: "bg-yellow-500",
-  },
-  {
+
+   {
     icon: <FaTrash className="text-4xl text-white" />,
-    title: "Trash Hauling for Apartments & Commercial Buildings",
+    title: "Cleaning Apartment & Commercial Buildings",
     description: "Reliable trash removal for clean and safe environments.",
     bg: "bg-red-500",
   },
+ 
+  {
+    icon: <FaDolly className="text-4xl text-white" />,
+    title: "Post Construction cleaning",
+    description: "Grind-N-Rise isn’t just hauling away debris, but also helping with post-construction cleaning tasks like pressure washing, interior window cleaning and site cleanup.",
+    bg: "bg-yellow-500",
+  },
+ 
 ];
 
 const Services = () => {
   return (
     <section
       id="services"
-      className="relative py-16 md:py-24 text-gray-900"
+      className="relative py-16 md:py-24 text-gray-900 inset-0 bg-black bg-opacity-70"
       style={{
         backgroundImage: `url(${servicesBg})`,
         backgroundSize: "cover",
@@ -43,12 +40,12 @@ const Services = () => {
       {/* Overlay */}
       <div className="relative inset-0 bg-black bg-opacity-60"></div>
 
-      <div className="relative container mx-auto px-6 md:px-12">
+      <div className="relative container mx-auto px-6 md:px-12 lg:left-30">
         <h2 className="text-3xl sm:text-4xl font-bold text-center text-white mb-12 mr-5">
           Our Services
         </h2>
 
-        <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+        <div className="grid  gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
           {services.map((service, index) => (
             <motion.div
               key={index}
