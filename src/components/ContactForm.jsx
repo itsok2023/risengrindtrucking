4eccf3756a9432bf0a3d55f8f4fcbb6cbfed5760
Vinay Satalkar <68776248+VINAYSATALKAR@users.noreply.github.com>
2025-08-25 11:44:32 +0530

    size of logo changed

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -142,7 +142,7 @@ function ContactPage() {
           <img
             src="/certification.png"
             alt="Certification Logo"
-            className="w-32 lg:w-80 lg:mt-50 lg:ml-10 h-auto mb-4"
+            className="w-60 lg:w-80 lg:mt-50 lg:ml-10 h-auto mb-4"
           />
           <p className="text-gray-700 font-medium text-lg lg:ml-10">
             NAICS: <span className="font-semibold">484119 / 441320</span>
